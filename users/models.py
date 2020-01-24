1f82210c0b16f6a8dff307d7285f552e5cd6fcf7
ricepotato <sj@mobills.co.kr>
2020-01-25 02:27:48 +0900

    user models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        max_length=10, null=True, choices=GENDER_CHOICES, blank=True
+    )
+    bio = models.TextField(default="")
