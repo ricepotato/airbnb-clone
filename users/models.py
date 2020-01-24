26f29bcc419cdd15d42ecc98effc502d74ab2d91
ricepotato <sj@mobills.co.kr>
2020-01-25 02:36:56 +0900

    update user model

--- users/models.py
+++ users/models.py
@@ -16,8 +16,29 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_WON = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_WON, "KRW"))
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         max_length=10, null=True, choices=GENDER_CHOICES, blank=True
     )
     bio = models.TextField(default="")
+    birthday = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
