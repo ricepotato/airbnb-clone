463eae0c5ca357dda3a6a2a545840af72bf509c1
ricepotato <ricepotato40@gmail.com>
2021-03-27 22:19:15 +0900

    filter form

--- rooms/views.py
+++ rooms/views.py
@@ -58,9 +58,40 @@ def search(request):
     room_type = int(request.GET.get("room_type", 0))
     room_types = models.RoomType.objects.all()
 
-    choices = {"countries": countries, "room_types": room_types}
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
-    form = {"city": city, "s_country": country, "s_room_type": room_type}
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
+
+    form = {
+        "city": city,
+        "s_country": country,
+        "s_room_type": room_type,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "instant": instant,
+        "super_host": super_host,
+    }
 
     return render(
         request,
